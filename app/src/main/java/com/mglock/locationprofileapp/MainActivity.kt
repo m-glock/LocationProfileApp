18e18bb6a2c902579125c3c00b11fe7941379012
m-glock <mareike.glock@t-online.de>
2022-03-20 16:28:05 +0100

    added names of tabs

--- app/src/main/java/com/mglock/locationprofileapp/MainActivity.kt
+++ app/src/main/java/com/mglock/locationprofileapp/MainActivity.kt
@@ -9,11 +9,6 @@ import com.mglock.locationprofileapp.ui.adapter.ViewPagerAdapter
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
-    private val animalsArray = arrayOf(
-        "Profiles",
-        "Places",
-        "Actions"
-    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,8 +22,14 @@ class MainActivity : AppCompatActivity() {
         val adapter = ViewPagerAdapter(supportFragmentManager, lifecycle)
         viewPager.adapter = adapter
 
+        val tabTitles = arrayOf(
+            getString(R.string.tab_title_actions),
+            getString(R.string.tab_title_profiles),
+            getString(R.string.tab_title_places)
+        )
         TabLayoutMediator(tabLayout, viewPager) { tab, position ->
-            tab.text = animalsArray[position]
+            tab.text = tabTitles[position]
         }.attach()
+        viewPager.currentItem = 1
     }
 }
