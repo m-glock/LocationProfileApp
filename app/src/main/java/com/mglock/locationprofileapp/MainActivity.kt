356df7d52097364a210d0a2cb7d2d41957c807f0
m-glock <mareike.glock@t-online.de>
2022-03-20 11:38:33 +0100

    basic layout and functions of a tab view with three tabs and swiping functionality

--- app/src/main/java/com/mglock/locationprofileapp/MainActivity.kt
+++ app/src/main/java/com/mglock/locationprofileapp/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.mglock.locationprofileapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.tabs.TabLayoutMediator
+import com.mglock.locationprofileapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    private val animalsArray = arrayOf(
+        "Profiles",
+        "Places",
+        "Actions"
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val viewPager = binding.viewPager
+        val tabLayout = binding.tabLayout
+
+        val adapter = ViewPagerAdapter(supportFragmentManager, lifecycle)
+        viewPager.adapter = adapter
+
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            tab.text = animalsArray[position]
+        }.attach()
     }
 }
---
+++ app/src/main/java/com/mglock/locationprofileapp/ViewPagerAdapter.kt
@@ -0,0 +1,27 @@
+package com.mglock.locationprofileapp
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.mglock.locationprofileapp.ui.fragments.ActionsFragment
+import com.mglock.locationprofileapp.ui.fragments.PlacesFragment
+import com.mglock.locationprofileapp.ui.fragments.ProfilesFragment
+
+private const val NUM_TABS = 3
+
+class ViewPagerAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle) :
+    FragmentStateAdapter(fragmentManager, lifecycle) {
+
+    override fun getItemCount(): Int {
+        return NUM_TABS
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        when (position) {
+            0 -> return ProfilesFragment()
+            1 -> return PlacesFragment()
+        }
+        return ActionsFragment()
+    }
+}
