7247d215d8a6ef1398efb3e050ce1ed79e2dcead
m-glock <mareike.glock@t-online.de>
2022-03-26 10:34:17 +0100

    implemented functionality for switch between manual and automatic add place

--- app/src/main/java/com/mglock/locationprofileapp/AddPlace.kt
+++ app/src/main/java/com/mglock/locationprofileapp/AddPlace.kt
@@ -2,7 +2,11 @@ package com.mglock.locationprofileapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import com.mglock.locationprofileapp.databinding.ActivityAddPlaceBinding
+import com.mglock.locationprofileapp.views.fragments.AddPlaceAutoFragment
+import com.mglock.locationprofileapp.views.fragments.AddPlaceManualFragment
 
 class AddPlace : AppCompatActivity() {
 
@@ -14,5 +18,24 @@ class AddPlace : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        val addPlaceManualFragment = AddPlaceManualFragment()
+        val addPlaceAutoFragment = AddPlaceAutoFragment()
+        setNewFragment(addPlaceManualFragment)
+
+        binding.automaticModeButton.setOnClickListener {
+            setNewFragment(addPlaceAutoFragment)
+        }
+
+        binding.manualModeButton.setOnClickListener {
+            setNewFragment(addPlaceManualFragment)
+        }
+    }
+
+    fun setNewFragment(fragment: Fragment){
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(binding.modeFragment.id, fragment)
+        fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+        fragmentTransaction.addToBackStack(null)
+        fragmentTransaction.commit()
     }
 }
