27d8c57aa3349d9a7d6722ec635896e2b2d455d3
m-glock <mareike.glock@t-online.de>
2022-04-01 16:30:22 +0200

    added bluetooth handler with some methods and added DetailActionOptions

--- app/src/main/java/com/mglock/locationprofileapp/util/enums/DetailActionOption.kt
+++ app/src/main/java/com/mglock/locationprofileapp/util/enums/DetailActionOption.kt
@@ -1,6 +1,8 @@
 package com.mglock.locationprofileapp.util.enums
 
 import android.Manifest
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import com.mglock.locationprofileapp.views.profiles.fragments.AddActionRingtoneFragment
 import com.mglock.locationprofileapp.views.profiles.fragments.AddActionVolumeModeFragment
@@ -22,12 +24,21 @@ enum class DetailActionOption(val title: String){
             return listOf(Manifest.permission.ACCESS_NOTIFICATION_POLICY)
         }
     },
-    WIFI("Wi-Fi"){
+    NOTIFY_BLUETOOTH_ENABLED("Bluetooth state notification"){
         override fun getRequiredPermissions(): List<String> {
-            TODO("Not yet implemented")
+            return listOf(Manifest.permission.BLUETOOTH)
         }
     },
-    BLUETOOTH("Bluetooth"){
+    NOTIFY_BLUETOOTH_DEVICE_CONNECTED("Bluetooth connected to Device"){
+        @RequiresApi(Build.VERSION_CODES.S)
+        override fun getRequiredPermissions(): List<String> {
+            return listOf(
+                Manifest.permission.BLUETOOTH,
+                Manifest.permission.BLUETOOTH_CONNECT
+            )
+        }
+    },
+    NOTIFY_WIFI_ENABLED("Wi-Fi state notification"){
         override fun getRequiredPermissions(): List<String> {
             TODO("Not yet implemented")
         }
@@ -41,6 +52,8 @@ enum class DetailActionOption(val title: String){
                 CHANGE_VOLUME_MODE -> AddActionVolumeModeFragment()
                 CHANGE_VOLUME -> AddActionVolumeFragment()
                 CHANGE_RINGTONE -> AddActionRingtoneFragment()
+                NOTIFY_BLUETOOTH_ENABLED -> Fragment()
+                NOTIFY_BLUETOOTH_DEVICE_CONNECTED -> Fragment()
                 else -> Fragment()
             }
         }
---
+++ app/src/main/java/com/mglock/locationprofileapp/util/phonefunctionality/BluetoothHandler.kt
@@ -0,0 +1,56 @@
+package com.mglock.locationprofileapp.util.phonefunctionality
+
+import android.annotation.SuppressLint
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothManager
+import android.content.Context
+import com.karumi.dexter.Dexter
+import com.mglock.locationprofileapp.util.PermissionListener
+import com.mglock.locationprofileapp.util.enums.DetailActionOption
+
+class BluetoothHandler(context: Context) {
+
+    private val bluetoothManager = context.getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+    // will be false unless permissions are granted
+    private var isBluetoothEnabled: Boolean = false
+
+    init {
+        Dexter.withContext(context)
+            .withPermissions(
+                DetailActionOption.NOTIFY_BLUETOOTH_DEVICE_CONNECTED.getRequiredPermissions()
+            ).withListener(PermissionListener(context){
+                isBluetoothEnabled = bluetoothManager.adapter.isEnabled
+            })
+            .check()
+    }
+
+    // get information
+    @SuppressLint("MissingPermission")
+    fun getBondedBluetoothDevices(): MutableSet<BluetoothDevice>?{
+        return if(isBluetoothEnabled){
+            val adapter = bluetoothManager.adapter
+            adapter.bondedDevices
+        } else {
+            null
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    fun getBondedBluetoothDeviceNames(): List<String>{
+        val bondedDevices = getBondedBluetoothDevices()
+        val bondedDeviceNames = bondedDevices?.map{ device -> device.name }
+
+        return bondedDeviceNames ?: emptyList()
+    }
+
+    // actions
+    fun isConnectedToDevice(): Boolean{
+        //TODO
+        return false
+    }
+
+    fun checkIfBluetoothEnabled(): Boolean{
+        return isBluetoothEnabled
+    }
+
+}
